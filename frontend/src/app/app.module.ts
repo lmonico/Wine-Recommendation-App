3ed3542c26f345671fb30330167ba7275b9000d8
Lukas Monico <lukas-monico@uiowa.edu>
2019-04-15 13:26:24 -0500

    Added in responsive navbar, uses sidebar if small window

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FlexLayoutModule } from '@angular/flex-layout'; //used for responsive nav-bar
+import {MatButtonModule, MatListModule, MatSidenavModule, MatToolbarModule} from "@angular/material";
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
