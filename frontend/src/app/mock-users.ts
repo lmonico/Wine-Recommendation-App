7ee7a32342722c2dcaa1782fdbaf69c73ab6391e
Lukas Monico <lukas-monico@uiowa.edu>
2019-04-17 12:32:53 -0500

    Following angular hero tutorial to learn more

---
+++ frontend/src/app/mock-users.ts
@@ -0,0 +1,8 @@
+import {User} from "./user"
+
+export const USERS: User[] = [
+  { id: 1, name: 'Lukas' },
+  { id: 2, name: 'Jason' },
+  { id: 3, name: 'Harsh' },
+  { id: 4, name: 'Diego' }
+  ];
