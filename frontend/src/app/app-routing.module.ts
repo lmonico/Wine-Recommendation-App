701b0a3227258bbcc7a4624d7e3bece658ba926a
Lukas Monico <lukas-monico@uiowa.edu>
2019-04-18 13:19:43 -0500

    Created route to users

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent }   from './dashboard/dashboard.component';
+import {UsersComponent} from "./users/users.component";
 
-const routes: Routes = [{ path: 'dashboard', component: DashboardComponent }];
+const routes: Routes = [ { path: 'users', component: UsersComponent },
+  { path: 'dashboard', component: DashboardComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
