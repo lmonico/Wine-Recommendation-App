4ab26bb303897084488e71835ec6bfdf647ca4a9
Lukas Monico <lukas-monico@uiowa.edu>
2019-04-18 14:24:49 -0500

    Changed scss back to sass since I understand sass syntax now

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -5,7 +5,7 @@ import { UserService } from '../user.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.scss' ]
+  styleUrls: [ './dashboard.component.sass' ]
 })
 export class DashboardComponent implements OnInit {
   users: User[] = [];
