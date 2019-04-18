a5e2df7397b080b7540e1d31db6e44b1de6e8669
Lukas Monico <lukas-monico@uiowa.edu>
2019-04-18 14:55:46 -0500

    Minor changes to about page, about routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent }   from './dashboard/dashboard.component';
-import {UsersComponent} from "./users/users.component";
+import { UsersComponent } from "./users/users.component";
 import { UserDetailComponent } from "./user-detail/user-detail.component";
-import {HomeComponent} from "./home/home.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
 
 const routes: Routes = [ { path: 'users', component: UsersComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', component: HomeComponent },
-  { path: 'detail/:id', component: UserDetailComponent }];
+  { path: 'detail/:id', component: UserDetailComponent },
+  { path: 'about', component: AboutComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
