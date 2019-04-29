4542924bfd76d7076f5476808b8484a455f7711a
Lukas Monico <lukas-monico@uiowa.edu>
2019-04-29 16:05:21 -0500

    Still doing review detail view

--- frontend/src/app/mock-reviews.ts
+++ frontend/src/app/mock-reviews.ts
@@ -1,9 +1,10 @@
 import {Review} from "./_models/review";
 
 export const REVIEWS: Review[] = [
-  {id: 123, country: "USA", description: "Temp",
+  {id: 123, country: "USA", description: "This wine is great!",
   designation: "temp", points: 100, price: 22.30,
   province: "CA", region_1: "n/a", region_2: "n/a",
   taster_name:"Me", taster_twitter_handle:"elonmusk",
-  variety: "Moscato", winery: "Local Vinyard", title: "temp"}
+  variety: "Moscato", winery: "Local Vinyard",
+    title: "Nicosia 2013 Vulkà Bianco (Etna)"}
 ];
