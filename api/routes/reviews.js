9c31666c79877a8c9c2c158bb72771721ef58294
Jason Ryan <jason-ryan@uiowa.edu>
2019-05-05 04:38:03 -0500

    Updated error message to include proper http status. Added get for single review.

--- api/routes/reviews.js
+++ api/routes/reviews.js
@@ -11,23 +11,22 @@ mongoose.connect('mongodb://localhost/big-wine', {useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connectionerror: '));
 db.once('open', function() {
-  
   autoIncremnet.initialize(db);
 
   var reviewSchema = new mongoose.Schema({
-      country: String,
-      description: String,
-      designation: String,
-      points: Int32,
-      price: String,
-      province: String,
-      region_1: String,
-      region_2: String,
-      taster_name: String,
-      taster_twitter_handle: String,
-      title: String,
-      variety: String,
-      winery: String
+    country: String,
+    description: String,
+    designation: String,
+    points: Int32,
+    price: String,
+    province: String,
+    region_1: String,
+    region_2: String,
+    taster_name: String,
+    taster_twitter_handle: String,
+    title: String,
+    variety: String,
+    winery: String
   });
   
   reviewSchema.plugin(autoIncremnet.plugin, {
@@ -36,16 +35,21 @@ db.once('open', function() {
     startAt: 129972
   });
   var Review = mongoose.model('Review', reviewSchema);
-});
+  
 
-// Error on no id
-router.get('/', function(req, res, next) {
-  res.status(404).send('Error: Please specify wine.');
-});
-
-// Get wine review
-router.get('/:id', function(req, res, next) {
-    res.send(req.params.id);
+  // Error no id
+  router.get('/', function(req, res, next) {
+      res.status(400).send('Please specify a reveiw id.')
+  });
+  
+  // GET wine review
+  router.get('/:id', function(req, res, next) {
+    var id = req.params.id;
+    Review.findById(id, function (err, review) {
+        if (err) return console.error(err);
+        res.send(review);
+    })
+  });
 });
 
 module.exports = router;
