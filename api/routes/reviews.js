78555302e243f52c893ab26ff1da793007619372
Jason Ryan <jason-ryan@uiowa.edu>
2019-05-05 04:21:42 -0500

    Mongoose connect with reviews database.

--- api/routes/reviews.js
+++ api/routes/reviews.js
@@ -2,14 +2,41 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
+var Int32 = require('mongoose-int32');
+var autoIncremnet = require('mongoose-auto-increment');
 var router = express.Router();
 
-mongoose.connect('mongodb://localhost/big-wine');
+// Connect to Mongo using Mongoose.
+mongoose.connect('mongodb://localhost/big-wine', {useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connectionerror: '));
 db.once('open', function() {
-    console.log('Connected');
-})
+  
+  autoIncremnet.initialize(db);
+
+  var reviewSchema = new mongoose.Schema({
+      country: String,
+      description: String,
+      designation: String,
+      points: Int32,
+      price: String,
+      province: String,
+      region_1: String,
+      region_2: String,
+      taster_name: String,
+      taster_twitter_handle: String,
+      title: String,
+      variety: String,
+      winery: String
+  });
+  
+  reviewSchema.plugin(autoIncremnet.plugin, {
+    model: 'Reveiw',
+    field: '_id',
+    startAt: 129972
+  });
+  var Review = mongoose.model('Review', reviewSchema);
+});
 
 // Error on no id
 router.get('/', function(req, res, next) {
